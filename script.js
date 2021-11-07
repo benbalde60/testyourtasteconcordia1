903f0677ed4bcce6213fe693bfa18e52fbc0157e
benbalde60 <64278953+benbalde60@users.noreply.github.com>
2021-11-07 11:56:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,3 @@
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-			const closeModalButtons = document.querySelectorAll('[data-close-button]')
-			const overlay = document.getElementByid('overlay')
-			
-			openModalButtons.forEach(button => {
-				button.addEventListener('click', () => {
-			const modal = document.querySelector(button.dataset.modalTarget)
-			openModal(modal)
-			    })
-                        })
-			overlay.addEventListener('click , () => {
-                            const modals = document.querySelectorAll('.modal.active')
-			modals.ForEach(model =>  {
-				closeModal(modal)
-			     })
-			})
-			closeModalButtons.forEach(button => {
-				button.addEvenetListener('click', () => { 
-	                          const modal = button.closest('.modal')
-				  closeModal(modal) 
-	                     )}
-			)}
-			function openModal(modal) { 
-				if(modal == null) return
-				modal.classList.add('active')
-				overlay.classList.add('active')
-			}
-			function closeModal(modal) {
-				if(modal == null) return
-				modal.classList.remove('active')
-				overlay.classList.remove('active')
-			}
+function togglePopup() {
+	document.getElementById("popup-1").classList.toggle("active);
+}
