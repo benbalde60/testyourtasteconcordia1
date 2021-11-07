1ef24eb478ad5791fb4fe0689da695730357a6d6
benbalde60 <64278953+benbalde60@users.noreply.github.com>
2021-11-07 12:16:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function togglePopup() {
-	document.getElementById("popup-1").classList.toggle("active);
+function togglePopup(){
+  let popup = document.getElementById("popup-1");
+  if(popup){
+    popup.classList.toggle("active");
+    if(!popup.classList.contains("active")){
+      popup.parentElement.removeChild(popup);
+    }
+  }
 }
