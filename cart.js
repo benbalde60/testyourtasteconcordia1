22983b8a62fb7725d41f0f04f9b2793001657934
benbalde60 <64278953+benbalde60@users.noreply.github.com>
2021-11-07 21:09:05 -0500

    Update cart.js

--- cart.js
+++ cart.js
@@ -4,6 +4,23 @@ for (var i =0; i < deleteButtons.length;i++) {
   button.addEventListener('click', function(event) {
         var buttonClicked = event.target
         buttonClicked.parentElement.parentElement.parentElement.parentElement.remove()
-                          
-                          })
- }
+        updateCartTotal() 
+  })
+}
+ function updateCartTotal() {
+         var cartItems = document.getElementsByClassName('cart')[0]
+         var cartRows = cartItems.getElementsByClassName('cart1')
+         var totalPrice
+         for(var i=0; i < cartRows.length;i++) {
+           var cartRow = cartRows[i]
+           var priceElement = cartRow.getElementsByClassName('price')[0]
+           var quantityElement = cartRow.getElementsByClassName('quantity')[0]
+           var price = parseFloat(priceElement.innerText.replace('$', ''))
+           var quantity = quantityElement.value
+           totalPrice = totalPrice + (price * quantity)
+           
+           
+                  }
+   document.getElementsByClassName('total-price')[0].innerText ='$'+ total
+         
+      
