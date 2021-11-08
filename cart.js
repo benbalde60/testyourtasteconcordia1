4e4f4a73260dd4f159e4c9111f659d7716de2c59
benbalde60 <64278953+benbalde60@users.noreply.github.com>
2021-11-07 19:39:25 -0500

    Update cart.js

--- cart.js
+++ cart.js
@@ -3,7 +3,7 @@ for (var i =0; i < deleteButtons.length;i++) {
   var button = deleteButtons[i]
   button.addEventListener('click', function(event)) {
         var buttonClicked = event.target
-        buttonClicked.parentElement.parentElement.remove()
+        buttonClicked.parentElement.parentElement.parentElement.parentElement.remove()
                           
                           }
  }
